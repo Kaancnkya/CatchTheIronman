e8eb2fab0155455258b7d11d5c19f932c5764afe
kaancnkya <kaancnkya@gmail.com>
2023-03-30 21:45:48 +0300

    Cath The Ironman

---
+++ app/src/main/java/com/example/catchtheironman/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.catchtheironman;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    TextView timeView;
+    TextView scoreView;
+    int score;
+
+    ImageView imageView;
+    ImageView imageView2;
+    ImageView imageView3;
+    ImageView imageView4;
+    ImageView imageView5;
+    ImageView imageView6;
+    ImageView imageView7;
+    ImageView imageView8;
+    ImageView imageView9;
+    ImageView[] imageArray;
+
+    Handler handler;
+    Runnable runnable;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timeView =  findViewById(R.id.timeView);
+        scoreView = findViewById(R.id.scoreView);
+        imageView = findViewById(R.id.imgView);
+        imageView2 = findViewById(R.id.imgView2);
+        imageView3 = findViewById(R.id.imgView3);
+        imageView4 = findViewById(R.id.imgView4);
+        imageView5 = findViewById(R.id.imgView5);
+        imageView6 = findViewById(R.id.imgView6);
+        imageView7 = findViewById(R.id.imgView7);
+        imageView8 = findViewById(R.id.imgView8);
+        imageView9 = findViewById(R.id.imgView9);
+        imageArray = new ImageView[] {imageView,imageView2,imageView3,imageView4,imageView5,imageView6,imageView7,imageView8,imageView9};
+        hideImages();
+        score = 0;
+        new CountDownTimer(10000,1000){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timeView.setText("Time: " + millisUntilFinished/1000);
+
+            }
+
+            @Override
+            public void onFinish() {
+                timeView.setText("Time Off");
+                handler.removeCallbacks(runnable);
+                for (ImageView image : imageArray ) {
+                    image.setVisibility(View.INVISIBLE);
+                }
+                AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);
+                alert.setTitle("RESTART?");
+                alert.setMessage("Are you sure to restart game ?");
+                alert.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = getIntent();
+                        finish();
+                        startActivity(intent);
+
+                    }
+                });
+                alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "GAME OVER!",Toast.LENGTH_LONG).show();
+                    }
+                });
+
+                alert.show();
+
+            }
+        }.start();
+    }
+
+    public void increaseScore(View view) {
+        score++;
+        scoreView.setText("Score: " + score);
+
+    }
+    public void hideImages(){
+        handler = new Handler();
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (ImageView image : imageArray ) {
+                    image.setVisibility(View.INVISIBLE);
+                }
+                Random random = new Random();
+                int i = random.nextInt(9);
+                imageArray[i].setVisibility(View.VISIBLE);
+                handler.postDelayed(this,500);
+
+            }
+        };
+
+        handler.post(runnable);
+
+    }
+}
